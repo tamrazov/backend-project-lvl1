1b0c6b54d485b86f1dc62d012807264429609263
Alex <box-570@yandex.ru>
2021-09-11 10:28:58 +0300

    fix workflows

--- src/cli.js
+++ src/cli.js
@@ -2,4 +2,4 @@ import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
-console.log(`Hi ${userName}!`);
+console.log(`Hello, ${userName}!`);
