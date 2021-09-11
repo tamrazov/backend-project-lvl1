cf48c4c2b491ae9eae08f58c8abb464bdfe08b87
Alex <box-570@yandex.ru>
2021-09-11 10:59:44 +0300

    Fix engine

--- src/engine.js
+++ src/engine.js
@@ -5,20 +5,22 @@ const COUNT_QUESTIONS = 3;
 const engine = (description, game) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi ${userName}!`);
+  console.log(`Hello, ${userName}!`);
   console.log(description);
 
   for (let i = 0; i < COUNT_QUESTIONS; i += 1) {
     const [answer, curAnswer] = game();
     if (answer == curAnswer) {
       console.log('Correct!');
+      continue;
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${curAnswer}'.`);
-      break;
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
   }
 
-  console.log(`Let's try again, ${userName}!`);
+  console.log(`Congratulations, ${userName}!`)
 };
 
 export default engine;
